2de7f361a12be2a0642744a827f4d4b576ef48e2
Pedro Rosario <pedrorosario.estrella@hotmail.com>
2020-07-19 12:00:20 -0400

    Limit amount of messages to 50

--- client/index.js
+++ client/index.js
@@ -1,20 +1,23 @@
-"use strict";
+'use strict';
 
 function addMessageToChat({ username, message, chatroom_id, date }) {
-  const chatEl = document.querySelector("#chat"),
-    messageEl = document.createElement("li"),
-    time = new Date(date).toLocaleTimeString()
+  const chatEl = document.querySelector('#chat'),
+    messageEl = document.createElement('li'),
+    time = new Date(date).toLocaleTimeString();
 
-  messageEl.setAttribute("class", username);
+  messageEl.setAttribute('class', username);
   messageEl.innerText = `${time} ${username}: ${message}`;
 
+  if (chatEl.childElementCount >= 50) {
+    chatEl.removeChild(chatEl.childNodes[0]);
+  }
   chatEl.appendChild(messageEl);
 }
 
 function postMessage(message, chatroom, username) {
-  return fetch("http://localhost:3000/messages", {
-    method: "POST",
-    headers: { "content-type": "application/json; charset=UTF-8" },
+  return fetch('http://localhost:3000/messages', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json; charset=UTF-8' },
     body: JSON.stringify({
       message,
       chatroom_id: chatroom,
@@ -24,31 +27,31 @@ function postMessage(message, chatroom, username) {
 }
 
 function getAllMessages(chatroom = 1) {
-  return fetch("http://localhost:3000/messages/chatroom/1").then((res) =>
+  return fetch('http://localhost:3000/messages/chatroom/1').then((res) =>
     res.json()
   );
 }
 
 // Create WebSocket connection.
-const socket = new WebSocket("ws://localhost:8080", "echo-protocol");
+const socket = new WebSocket('ws://localhost:8080', 'echo-protocol');
 
 getAllMessages(1).then((messages) =>
   messages.forEach((m) => addMessageToChat(m))
 );
 
 // Listen for messages
-socket.addEventListener("message", (event) =>
+socket.addEventListener('message', (event) =>
   addMessageToChat(JSON.parse(event.data))
 );
 
-const txtbox = document.querySelector("input");
+const txtbox = document.querySelector('input');
 
-txtbox.addEventListener("keypress", (e) => {
-  if (e.code !== "Enter") return;
+txtbox.addEventListener('keypress', (e) => {
+  if (e.code !== 'Enter') return;
 
-  return postMessage(txtbox.value, 1, "bot")
+  return postMessage(txtbox.value, 1, 'bot')
     .then(() => {
-      txtbox.value = "";
+      txtbox.value = '';
     })
     .catch((error) => alert(`Message could not be sent. ${error.message}`));
 });
